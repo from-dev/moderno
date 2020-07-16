db1129212d9a61fd402ac284aaa8b2581ae7360a
from-dev <forbredd@gmail.com>
2020-07-16 20:24:19 +0300

    added slider and produkts

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
 
+$(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
+$('.produkt-slider__inner').slick({
+	dots: true,
+	arrows: false,
+	slidesToShow: 4,
+	slidesToScroll: 4
+});
+
+var mixer = mixitup('.produkts__inner-box');
 
 
 });
