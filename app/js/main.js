a25bfdcfe8407b3701e8f3b5db0d93eee85019c2
from-dev <forbredd@gmail.com>
2020-08-11 06:13:57 +0300

    added adapt header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,14 +7,44 @@ $(".rate-star").rateYo({
     readOnly: true
   });
 
-$('.produkt-slider__inner').slick({
+$('.product-slider__inner').slick({
 	dots: true,
 	arrows: false,
 	slidesToShow: 4,
 	slidesToScroll: 4
 });
 
-var mixer = mixitup('.produkts__inner-box');
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+
+$('.icon-th-list').on('click', function(){
+    $('.products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+});
+$('.icon-th-large').on('click', function(){
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+});
+
+
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+});
+
+$('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+});
+
+var mixer = mixitup('.products__inner-box');
 
 
 });
